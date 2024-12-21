42abadca4521bf738757c52fde018324f2007201
Erdinç <Erdin_uyar@hotmail.com>
2024-12-21 17:49:26 +0300

    CatchTheKennyGame

---
+++ CatchTheKennyGame/CatchTheKennyGameTests/CatchTheKennyGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyGameTests.swift
+//  CatchTheKennyGameTests
+//
+//  Created by Erdinç Uyar on 27.11.2024.
+//
+
+import Testing
+@testable import CatchTheKennyGame
+
+struct CatchTheKennyGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
